844d604cc832d9fb94d3a273d07c3a0f5e8a1875
Rohan <rjha09307@gmail.com>
2026-04-15 18:06:11 +0530

    update import

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,6 +1,6 @@
 import "./globals.css";
 import { AuthProvider } from "./context/AuthContext";
-import { Analytics } from "@vercel/analytics/react";
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata = {
   title: "resumeX | Smart Resume Management",
