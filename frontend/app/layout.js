19b71d41c8a665b6b3424327ae4d360be891d275
Rohan <rjha09307@gmail.com>
2026-06-27 15:29:55 +0530

    added speed insights

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { AuthProvider } from "./context/AuthContext";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata = {
   title: "resumeX",
@@ -18,6 +19,7 @@ export default function RootLayout({ children }) {
       <body>
         <AuthProvider>{children}</AuthProvider>
         <Analytics />
+        <SpeedInsights/>
       </body>
     </html>
   );
