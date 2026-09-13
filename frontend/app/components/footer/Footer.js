762eaaf3a97cb00046595b00012f8c45fa5d203b
Saanvi019 <saanvi.s019@gmail.com>
2026-09-13 11:16:21 +0530

    chore: remove blog article links from footer, keep top-level Blog link

--- frontend/app/components/footer/Footer.js
+++ frontend/app/components/footer/Footer.js
@@ -79,24 +79,9 @@ export default function Footer() {
             >
               Blog
             </a>
-            <a
-              href="/blog/how-to-create-a-resume-link"
-              className="block w-fit text-[#557083] transition-all duration-300 hover:translate-x-1.5 hover:text-[#123F5B]"
-            >
-              Create a Resume Link
-            </a>
-            <a
-              href="/blog/how-to-share-a-resume-online"
-              className="block w-fit text-[#557083] transition-all duration-300 hover:translate-x-1.5 hover:text-[#123F5B]"
-            >
-              Share a Resume Online
-            </a>
-            <a
-              href="/blog/how-to-send-a-resume-to-a-recruiter"
-              className="block w-fit text-[#557083] transition-all duration-300 hover:translate-x-1.5 hover:text-[#123F5B]"
-            >
-              Send Resume to Recruiter
-            </a>
+
+
+
           </div>
 
           <div className="space-y-1 text-sm font-medium">
